6f522211a2f2a3f7086a63930d551293d460069e
Филиппов Иван Александрович <Ione@main.sgu.ru>
2023-10-04 12:54:44 +0400

    Вторая работа

--- number2/main.py
+++ number2/main.py
@@ -6,5 +6,20 @@ def random_string(length):
     rand_string = ''.join(random.choice(letters) for i in range(length))
     return rand_string
 
+def check_chars(str1, str2):
+    seen = set()
+    for char in str1:
+        seen.add(char)
+    
+    result = set()
+    for char in str2:
+        if char in seen:
+            result.add(char)
 
-print("Создадим строку ")
+    return result
+
+
+str1 = random_string(5)
+str2 = random_string(5)
+print("Создадим две строки на 5 элементов:", str1, "и", str2)
+print("Одинаковые символы:", ''.join(check_chars(str1, str2)))
